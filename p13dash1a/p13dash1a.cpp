60e7bc4265e2f0e3eedadd0d754ebe024a463b86
Caleb Collert <c-collert.1@onu.edu>
2024-05-08 21:14:17 -0400

    finished project

--- p13dash1a/p13dash1a.cpp
+++ p13dash1a/p13dash1a.cpp
@@ -11,16 +11,20 @@ void countAlphabetFrequency(string& filename, double letterFreq[]);
 int main() {
 
     double letterFreq[26] = {};
+    double totalLetters = 0;
 
     string filename;
     cout << "Enter the name of the file: ";
     cin >> filename;
 
     countAlphabetFrequency(filename, letterFreq);
-
+    for (int i = 0;i < 26;i++) {
+        totalLetters += letterFreq[i];
+    }
+    cout << "Total letters: " << totalLetters << endl;
     cout << "Alphabet Letter Frequency:\n";
     for (char letter = 'a'; letter <= 'z'; ++letter) {
-        cout << letter << ": " << letterFreq[letter - 'a'] << endl;
+        cout << letter << ": " << (letterFreq[letter - 'a'] / totalLetters) * 100 << "%" << endl;
     }
 
     pressEnterToContinue();
